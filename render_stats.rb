b6d04347c3e5c1bf810243d988bcd1e2d1ae7019
Bartosz Dziewoński <matma.rex@gmail.com>
2020-09-29 01:45:42 +0200

    More rows in stats

--- render_stats.rb
+++ render_stats.rb
@@ -4,13 +4,18 @@ require 'cgi'
 require 'date'
 
 def html name, text = nil, **attrs, &contents
-	attrs_str = attrs.map{|k,v| " #{CGI.escapeHTML k.to_s}=\"#{CGI.escapeHTML v.to_s}\"" }.join ''
+	attrs_str = attrs.select{|k,v| v}.map{|k,v| " #{CGI.escapeHTML k.to_s}=\"#{CGI.escapeHTML v.to_s}\"" }.join ''
 	s = ''
 	s += "<#{CGI.escapeHTML name}#{attrs_str}>" if name
 	s += CGI.escapeHTML text.to_s if text
 	s += contents.call.to_s if contents
 	s += "</#{CGI.escapeHTML name}>" if name
 	return s
+end
+
+def percent a, b
+	return '' if b === 0
+	return "#{(a.to_f/b*100).round(1)}%"
 end
 
 database = JSON.parse File.read('database.json'), symbolize_names: true
@@ -39,6 +44,22 @@ Date.strptime(oldest_rev).upto(Date.today) do |day|
 	end
 end
 
+puts html('p', "Choose rows:")
+row_info = {
+	'sus' => true,
+	'good' => false,
+	'total' => true,
+	'suspc' => false,
+}
+row_info.each do |rowtype, active|
+	puts html('style'){ "tr.#{rowtype} > *:not([rowspan]) { display: none; }" }
+	onchange = 'this.parentNode.previousElementSibling.disabled = this.checked;'
+	puts html('label'){
+		html('input', type: 'checkbox', checked: active, onchange: onchange) + ' ' +
+		html(nil, rowtype)
+	}
+	puts html('script'){ "document.currentScript.previousElementSibling.previousElementSibling.disabled = #{active};" }
+end
 
 puts '<table class="wikitable">'
 puts '<tr>'
@@ -48,29 +69,55 @@ puts html('th', "All days", class: 'summary')
 puts '</tr>'
 
 rows.each do |site, data|
-	puts '<tr>'
-	puts html('th', site, rowspan: 2)
+	suspicious = data.map{|d| d[:suspicious] }.inject(:+)
+	total = data.map{|d| d[:total] }.inject(:+)
+
+	puts '<tr class="sus">'
+	puts html('th', site, rowspan: 4)
 	puts html('th', "sus")
 	data.each{|d| puts html('td', d[:suspicious]) }
-	puts html('td', data.map{|d| d[:suspicious] }.inject(:+))
+	puts html('td', suspicious)
 	puts '</tr>'
-	puts '<tr>'
+	puts '<tr class="good">'
+	puts html('th', "good")
+	data.each{|d| puts html('td', d[:total] - d[:suspicious]) }
+	puts html('td', total - suspicious)
+	puts '</tr>'
+	puts '<tr class="total">'
 	puts html('th', "total")
 	data.each{|d| puts html('td', d[:total]) }
-	puts html('td', data.map{|d| d[:total] }.inject(:+))
+	puts html('td', total)
+	puts '</tr>'
+	puts '<tr class="suspc">'
+	puts html('th', "suspc")
+	data.each{|d| puts html('td', percent(d[:suspicious], d[:total] )) }
+	puts html('td', percent(suspicious, total) )
 	puts '</tr>'
 end
 
-puts '<tr>'
-puts html('th', "All sites", rowspan: 2, class: 'summary')
+suspicious = headers.length.times.map{|i| rows.map{|site, r| r[i][:suspicious] }.inject(:+) }
+total = headers.length.times.map{|i| rows.map{|site, r| r[i][:total] }.inject(:+) }
+
+puts '<tr class="sus">'
+puts html('th', "All sites", rowspan: 4, class: 'summary')
 puts html('th', "sus", class: 'summary')
-headers.length.times{|i| puts html('td', rows.map{|site, r| r[i][:suspicious] }.inject(:+) ) }
-puts html('td', headers.length.times.map{|i| rows.map{|site, r| r[i][:suspicious] }.inject(:+) }.inject(:+), class: 'summary')
+suspicious.each{|s| puts html('td', s) }
+puts html('td', suspicious.inject(:+), class: 'summary')
 puts '</tr>'
-puts '<tr>'
+puts '<tr class="good">'
+puts html('th', "good", class: 'summary')
+suspicious.zip(total).each{|s, t| puts html('td', t - s) }
+puts html('td', total.inject(:+) - suspicious.inject(:+), class: 'summary')
+puts '</tr>'
+puts '<tr class="total">'
 puts html('th', "total", class: 'summary')
-headers.length.times{|i| puts html('td', rows.map{|site, r| r[i][:total] }.inject(:+) ) }
-puts html('td', headers.length.times.map{|i| rows.map{|site, r| r[i][:total] }.inject(:+) }.inject(:+), class: 'summary')
+total.each{|s| puts html('td', s) }
+puts html('td', total.inject(:+), class: 'summary')
+puts '</tr>'
+puts '<tr class="suspc">'
+puts html('th', "suspc", class: 'summary')
+suspicious.zip(total).each{|s, t| puts html('td', percent(s, t)) }
+puts html('td', percent(suspicious.inject(:+), total.inject(:+)), class: 'summary')
 puts '</tr>'
 
 puts '</table>'
