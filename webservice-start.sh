d6d12c582e20720e3437dd257f1b11f050d56147
Bartosz Dziewoński <matma.rex@gmail.com>
2022-03-22 01:28:52 +0100

    Update webservice from Stretch to Buster

--- webservice-start.sh
+++ webservice-start.sh
@@ -1,2 +1,5 @@
 # https://phabricator.wikimedia.org/T215599#4941087
-webservice --backend=gridengine lighttpd start
+# https://wikitech.wikimedia.org/wiki/News/Toolforge_Stretch_deprecation#Move_a_grid_engine_webservice
+# Can't use Kubernetes, because there's no configuration that allows both
+# serving static HTML files using lighttpd and running CGI scripts using Ruby.
+webservice --backend=gridengine --release buster lighttpd start
