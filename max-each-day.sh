8d38ee5ce6540715f8d28d12bc9f56b067edf1e7
Bartosz Dziewoński <matma.rex@gmail.com>
2023-05-07 22:20:56 +0200

    Check number of sites processed too

--- max-each-day.sh
+++ max-each-day.sh
@@ -1,1 +1,1 @@
-sqlite3 -column database.sqlite "select date(timestamp), max( time(timestamp) ) from siterevs group by date(timestamp)"
+sqlite3 -column database.sqlite "select date(timestamp), max( time(timestamp) ), count(distinct site) from siterevs group by date(timestamp)"
